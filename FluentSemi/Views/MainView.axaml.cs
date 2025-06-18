290b21558f4df718326706c35af9f8fa437a0d06
codeCLown <zhaomengshou@126.com>
2025-06-18 18:38:09 +0800

    update

--- FluentSemi/Views/MainView.axaml.cs
+++ FluentSemi/Views/MainView.axaml.cs
@@ -1,6 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Controls.Notifications;
-using FluentAvalonia.UI.Controls;
 using FluentSemi.ViewModels;
 
 namespace FluentSemi.Views;
@@ -13,14 +11,5 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
         DataContext = ViewModel;
-
-        Nav.SelectionChanged += (_, e) =>
-        {
-            // ViewModel.TogglePage(((e.SelectedItem as NavigationViewItem).Tag as string)!);
-            App.Notification.Show(((e.SelectedItem as NavigationViewItem).Tag as string)!, NotificationType.Success);
-        };
     }
-
-    public NavigationView NavigationView => Nav;
-    public ContentControl Frame => FrameView;
 }
